07bf5e0fa896e339f767eebebd799a400724b234
Malon Holmes <itsmalonholmes@gmail.com>
2020-07-17 13:25:36 -0400

    SavingsAcct finished

---
+++ src/test/java/com/codedifferently/bankaccountlab/SavingsAccountTest.java
@@ -0,0 +1,19 @@
+package com.codedifferently.bankaccountlab;
+
+import org.junit.Test;
+
+import java.time.LocalDate;
+
+public class SavingsAccountTest {
+
+    @Test
+    public void chargeAnnualTest(){
+        // Given
+        //SavingsAccount sa = new SavingsAccount(1,"Bob",100.0,
+        //        LocalDate.of(2019, 7, 17));
+
+        // When
+
+        // Then
+    }
+}
